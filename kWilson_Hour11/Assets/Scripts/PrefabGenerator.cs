ae0391ebbf46231eefa14f13a486f43c080a7dc5
KwaliW <coastercop1@gmail.com>
2026-05-11 03:29:29 -0700

    Made the posts spawn in a line, and changed the camera position so we can see it

--- kWilson_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ kWilson_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -1,22 +1,35 @@
 using UnityEngine;
 
 public class PrefabGenerator : MonoBehaviour
-
 {
-
     public GameObject prefab;
 
-    void Update()
-    { // Whenever we hit the B key we will generate a prefab at the // position of the original prefab // Whenever we hit the space key, we will generate a prefab at the // position of the spawn object that this script is attached to if (Input .GetKeyDown (KeyCode.B) )
+    
+    Vector3 spawnPoint = new Vector3(-1f, 2f, -5f);
 
+    void Start()
+    {
+        
+        for (int i = 0; i < 10; i++)
         {
+            Vector3 spawnPosition = spawnPoint + new Vector3(i * 3f, 0f, 0f);
 
-            Instantiate(prefab);
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+    }
 
-            if (Input.GetKeyDown(KeyCode.Space))
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(prefab, spawnPoint, Quaternion.identity);
+        }
 
-                Instantiate(prefab, transform.position, transform.rotation);
-
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
         }
     }
 }
