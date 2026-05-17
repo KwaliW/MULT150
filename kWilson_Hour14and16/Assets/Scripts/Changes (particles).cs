f88d8fb52577a8ae5442ef73e5a6ad6e9fae0018
KwaliW <coastercop1@gmail.com>
2026-05-17 16:30:53 -0700

    Made new particles, changed how those particles interact with a sphere

---
+++ kWilson_Hour14and16/Assets/Scripts/Changes (particles).cs
@@ -0,0 +1,5 @@
+//Made the particle change color over time
+//Made the particle change color the faster that it got
+//Added a ribbon to the attached particles
+//Changed the cuve for the angular rotation
+//Adjusted the curve for the speed of which the particles emit
