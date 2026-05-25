9443b567efef1975ac783fef67bc41b753a6a25d
KwaliW <coastercop1@gmail.com>
2026-05-24 20:39:11 -0700

    Made the gauntlet runner game, changed both prefabs to make the game harder!

---
+++ kWilson_Hour20/Assets/Scripts/Collidable.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Collidable : MonoBehaviour
+{
+    public GameManager manager;
+
+    public float moveSpeed = 20f;
+    public float timeAmount = 1.5f;
+
+    void Update()
+    {
+        transform.Translate(
+            0,
+            0,
+            -moveSpeed * Time.deltaTime
+        );
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            manager.AdjustTime(timeAmount);
+
+            Destroy(gameObject);
+        }
+    }
+}
